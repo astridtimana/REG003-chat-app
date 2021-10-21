8c5e285585f9aa7a7e294de4c01dd784a35bb79d
astridtimana <astrid.1295@gmail.com>
2021-10-21 13:13:04 -0500

    server + sockets

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,28 +1,37 @@
 import express, { Application } from 'express';
+import http from 'http';
 import cors from 'cors';
 import userRoutes from '../routes/user';
 import authRoutes from '../routes/auth';
 import chatRoutes from '../routes/chat';
 import errorMiddleware from '../middlewares/errorHandler';
 import cookieParser from 'cookie-parser';
-// import * as pkg from '../../package.json'
+import { Server  as SocketServer  } from "socket.io";
 
 class Server {
   public app: Application;
   public port: String;
+  public server: any;
+  public io: any;
   public apiPaths = {
+    auth: '/',
     users: '/users',
-    auth: '/auth',
     chat: '/chat',
   }
+  
+
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT || '8080';
+
+    this.server = http.createServer(this.app);
     
-    this.middlewares();
-    this.routes();
-    // this.app.set('pkg', pkg);
+    this.app.set("trust proxy", 1); // es necesario?
+    
+    // Configuraciones de sockets
+    this.io = new SocketServer( this.server);
+
   }
 
   middlewares() {
@@ -36,18 +45,33 @@ class Server {
     this.app.use(express.urlencoded({ extended: false }));
     this.app.use(errorMiddleware);
   }
-
+  
   routes() {
-    this.app.use(this.apiPaths.users, userRoutes)
     this.app.use(this.apiPaths.auth, authRoutes)
+    this.app.use(this.apiPaths.users, userRoutes)
     this.app.use(this.apiPaths.chat, chatRoutes)
   }
 
-  listen() {
-    this.app.listen(this.port, () => {
-      console.log(`Servidor corriendo en puerto ${this.port}`);
-    })
+  execute() {
+
+    // Inicializar Middlewares
+    this.middlewares();
+    this.routes();
+
+    // Inicializar sockets
+    //this.configurarSockets();
+
+    // Inicializar Server
+    this.server.listen( this.port, () => {
+        console.log('Server corriendo en puerto', this.port );
+    });
   }
+
+  // listen() {
+  //   this.app.listen(this.port, () => {
+  //     console.log(`Servidor corriendo en puerto ${this.port}`);
+  //   })
+  // }
 };
 
 export default Server;
