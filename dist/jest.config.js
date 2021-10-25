6b0cbf7987748ea7dd09fc401b13b1a174a6c463
Thais Illescas <thainole@gmail.com>
2021-10-25 13:54:49 -0500

    localstorage

--- dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    clearMocks: true,
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    setupFilesAfterEnv: ['<rootDir>/test/singleton.ts'],
-};
-//# sourceMappingURL=jest.config.js.map
